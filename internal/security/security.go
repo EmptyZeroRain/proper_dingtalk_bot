ce47e6626407a903201c0dff5dcbdaebd011d133
空之回响 <rain@dfrain.com>
2024-08-28 14:14:33 +0800

    Update security.go

--- internal/security/security.go
+++ internal/security/security.go
@@ -14,6 +14,7 @@ import (
 
 // https://oapi.dingtalk.com/robot/send?access_token=xxx
 // const dingTalkOAPI = "oapi.dingtalk.com"
+const dingTalkOAPI = "zd-openplatform.bigdatacq.com"
 
 var dingTalkURL url.URL = url.URL{
 	Scheme: "https",
